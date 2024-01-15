a4d51a0e30bdbad1f544dc7c85df29dbfd9670aa
Tibor Mirnic <mirnictibor@gmail.com>
2024-01-15 20:13:02 +0100

    build: migrate nx to 17.2.8

--- vite.config.ts
+++ vite.config.ts
@@ -4,16 +4,24 @@ import react from '@vitejs/plugin-react';
 import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
 
 export default defineConfig({
+  root: __dirname,
+  build: {
+    outDir: 'dist/goto-react',
+    reportCompressedSize: true,
+    commonjsOptions: {
+      transformMixedEsModules: true
+    }
+  },
   cacheDir: './node_modules/.vite/goto-react',
 
   server: {
     port: 4200,
-    host: 'localhost',
+    host: 'localhost'
   },
 
   preview: {
     port: 4300,
-    host: 'localhost',
+    host: 'localhost'
   },
 
   plugins: [react(), nxViteTsPaths()],
@@ -24,11 +32,16 @@ export default defineConfig({
   // },
 
   test: {
+    reporters: ['default'],
+    coverage: {
+      reportsDirectory: 'coverage/goto-react',
+      provider: 'v8'
+    },
     globals: true,
     cache: {
-      dir: './node_modules/.vitest',
+      dir: './node_modules/.vitest'
     },
     environment: 'jsdom',
-    include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
-  },
+    include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}']
+  }
 });
